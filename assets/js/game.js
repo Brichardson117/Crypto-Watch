0f396633cb7df6dbb35de42d7f97ce0cbe9fe24c
Brittney <brittneyrichardson8@gmail.com>
2022-04-05 18:23:07 -0500

    added get api function for cyrptos and button variables for main game page

---
+++ assets/js/game.js
@@ -0,0 +1,37 @@
+//Brittney start
+var startMoney = "$10,000";
+var money = " ";
+var buyBtn = document.getElementById("trade");
+var sellBtn = document.getElementById("sell")
+
+var endGame = function() {
+if (money <= startMoney) {
+    console.log("You did not become a crypto-Millionaire! You lose!")
+} else {
+    console.log("Congratulations, You Win!")
+};
+
+localStorage.setItem("Player's Money", money)
+
+var playAgain = window.confirm("Do you want to play again?") 
+ if (playAgain === true) {
+     startGame();
+ } else {
+     window.location.replace(" ")
+ }
+};
+
+var getCryptos = function() {
+    var cryptoApi = " "
+    fetch(cryptoApi)
+    .then(function(response){
+        if(response.ok) {
+            response.json().then(function(data){
+                displayCryptos()
+            })
+        } else {
+            alert("Unable to get Cryptos at this time")
+        }
+    })
+}
+// Brittney end
