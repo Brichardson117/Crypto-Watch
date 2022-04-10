1568db005b7de0eb20da08c8bbcd3229d6128bc5
Brittney <brittneyrichardson8@gmail.com>
2022-04-10 10:37:31 -0500

    added remove and add to watchlist functions and event listeners for both

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,9 +1,38 @@
 //Brittney start
 const startMoney = "$10,000";
 var money = " ";
-var buyBtn = document.getElementById("trade");
-var sellBtn = document.getElementById("sell")
+var buyBtn = document.getElementById("#trade");
+var sellBtn = document.getElementById("#sell")
+var removeBtn = document.getElementById("#remove")
+var addBtn = document.getElementById("#add")
 
+var getCryptoRepo = function() { 
+const baseUrl = "";
+      const endpoint = '';
+      const url = baseUrl + endpoint;
+      function apiCall() {
+        fetch(url)
+          .then(function (response) {
+            return response.json();
+          })
+          .then(function (data) {
+            console.log(data);
+            document.getElementById('root').textContent = data;
+          });
+      }
+      apiCall()
+}
+addBtn.addEventListener("click", addWatchlist)
+var addWatchlist = function() {
+    
+}
+
+ removeBtn.addEventListener("click", removeWatchlist)
+
+var removeWatchlist = function() {
+   const element = document.getElementById(" ");
+   element.remove()
+}
 var endGame = function() {
 if (money <= startMoney) {
     console.log("You did not become a crypto-Millionaire! You lose!")
@@ -34,5 +63,7 @@ var getCryptos = function() {
         }
     })
 }
+
+
 // event.preventDefault()
 // Brittney end
