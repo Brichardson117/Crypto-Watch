7a330a93c58a27e1ebaca0b968e13be91c4a6097
Brittney <brittneyrichardson8@gmail.com>
2022-04-13 14:32:08 -0500

    added watchlist function, removed game functions

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,39 +1,33 @@
 //Brittney start
-const startMoney = "$10,000";
-var money = " ";
-var buyBtn = document.getElementById("trade");
-var sellBtn = document.getElementById("sell");
 
-var endGame = function() {
-if (money <= startMoney) {
-    console.log("You did not become a crypto-Millionaire! You lose!")
-} else {
-    console.log("Congratulations, You Win!")
+var addBtn = document.getElementById("#add");
+
+
+
+//  addBtn.addEventListener("click", buildWatchList)
+var buildWatchList = function() {
+  var CryptoName = document.createElement("p");
+  CryptoName.textContent = data.data[0].name;
+  var marketCap = document.createElement("p");
+  marketCap.textContent = data.data[0].marketCapUsd;
+ var cryptoPrice = documnt.createElement("p");
+ cryptoPrice.textContent = data.data[0].priceUsd
+
 };
 
-localStorage.setItem("Player's Money", money)
-
-var playAgain = window.confirm("Do you want to play again?") 
- if (playAgain === true) {
-     startGame();
- } else {
-     window.location.replace(" ")
- }
-};
-
-var getCryptos = function() {
-    var cryptoApi = " "
-    fetch(cryptoApi)
-    .then(function(response){
-        if(response.ok) {
-            response.json().then(function(data){
-                displayCryptos()
-            })
-        } else {
-            alert("Unable to get Cryptos at this time")
-        }
-    })
-}
+// var getCryptos = function() {
+//     var cryptoApi = " "
+//     fetch(cryptoApi)
+//     .then(function(response){
+//         if(response.ok) {
+//             response.json().then(function(data){
+//                 displayCryptos()
+//             })
+//         } else {
+//             alert("Unable to get Cryptos at this time")
+//         }
+//     })
+// }
 
 const baseUrl = 'https:api.coincap.io';
       const endpoint = '/v2/assets';
@@ -64,5 +58,6 @@ const baseUrl = 'https:api.coincap.io';
           });
       }
       apiCall();
-// event.preventDefault()
-// Brittney end
+
+
+      
